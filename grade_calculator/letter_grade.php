20c304f40c3b3457089017f3288185940d0e43e9
Owen Degnan <oxd404@francis.edu>
2026-04-29 21:01:21 -0400

    Cleaned up duplicate folder

---
+++ grade_calculator/letter_grade.php
@@ -0,0 +1,14 @@
+<?php
+function get letter_grade($grade){
+if($grade >= 90){
+Return "A"
+elseif ($grade >=89){
+Return "B"
+elseif ($grade >=79){
+Return "C"
+elseif ($grade >=69){
+Return "D"
+else{
+Return "F"
+}
+}
